fb2a5f925904582cfec8a4f6a06c04a9445017f9
zhanghao <zhanghao997@qq.com>
2024-06-26 13:58:21 +0800

    add tests case

--- tests/external_cass.sh
+++ tests/external_cass.sh
@@ -9,7 +9,7 @@ sed -i "s|monograph_keyspace_name=eloqsql|monograph_keyspace_name=waiter_${OS_ID
 sed -i "s|#cass_keyspace=eloqkv|cass_keyspace=waiter_${OS_ID}|" ${CLUSTER_MGR_HOME}/config/eloqkv.ini
 sed -i "s|enable_data_store=none|enable_data_store=all|" ${CLUSTER_MGR_HOME}/config/eloqkv.ini
 
-cluster_mgr demo eloq-sql --skip-deps --ext-cass ${CASSANDRA_ADDR}
+cluster_mgr demo eloq-sql --skip-deps --unlimited --ext-cass ${CASSANDRA_ADDR}
 CLIENT=$(cluster_mgr -q connect demo-sql-cassandra)
 cluster_mgr status demo-sql-cassandra --wait 5
 eval "${CLIENT} --execute 'SHOW DATABASES'"
@@ -18,12 +18,13 @@ cluster_mgr stop demo-sql-cassandra --all
 cluster_mgr remove demo-sql-cassandra
 
 sleep 15
-cluster_mgr demo eloq-kv --skip-deps --ext-cass ${CASSANDRA_ADDR}
+cluster_mgr demo eloq-kv --skip-deps --unlimited --ext-cass ${CASSANDRA_ADDR}
 CLIENT=$(cluster_mgr -q connect demo-kv-cassandra)
 cluster_mgr status demo-kv-cassandra --wait 5
 eval ${CLIENT} incr mycounter
-cluster_mgr monitor demo-kv-cassandra stop
-cluster_mgr stop demo-kv-cassandra --all
+cluster_mgr restart demo-kv-cassandra --all
+cluster_mgr status demo-kv-cassandra --wait 5
+eval ${CLIENT} incr mycounter
 cluster_mgr remove demo-kv-cassandra
 
 echo "External cassandra/scylla tests PASSED !!!"
