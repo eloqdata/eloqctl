396d6eaca06cae8aa28890b498f229d5abc84445
zhanghao <zhanghao997@qq.com>
2024-06-28 21:46:50 +0800

    CI: drop keyspace if exist

--- tests/external_cass.sh
+++ tests/external_cass.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -exo pipefail
 
-CASSANDRA_ADDR=$1
-source /etc/os-release
-OS_ID=${ID}$(echo ${VERSION_ID}| tr -d '.')
 echo ">>> Test external cassandra"
-sed -i "s|monograph_keyspace_name=eloqsql|monograph_keyspace_name=waiter_${OS_ID}|" ${CLUSTER_MGR_HOME}/config/eloqsql.ini
-sed -i "s|#cass_keyspace=eloqkv|cass_keyspace=waiter_${OS_ID}|" ${CLUSTER_MGR_HOME}/config/eloqkv.ini
+CASSANDRA_HOST=$1
+source /etc/os-release
+KEYSPACE_NAME="waiter_${ID}${VERSION_ID%.*}"
+cqlsh ${CASSANDRA_HOST} -e "DROP KEYSPACE IF EXISTS ${KEYSPACE_NAME}"
+
+sed -i "s|monograph_keyspace_name=eloqsql|monograph_keyspace_name=${KEYSPACE_NAME}|" ${CLUSTER_MGR_HOME}/config/eloqsql.ini
+sed -i "s|#cass_keyspace=eloqkv|cass_keyspace=${KEYSPACE_NAME}|" ${CLUSTER_MGR_HOME}/config/eloqkv.ini
 sed -i "s|enable_data_store=none|enable_data_store=all|" ${CLUSTER_MGR_HOME}/config/eloqkv.ini
 
-cluster_mgr demo eloq-sql --skip-deps --unlimited --ext-cass ${CASSANDRA_ADDR}
+cluster_mgr demo eloq-sql --skip-deps --unlimited --ext-cass ${CASSANDRA_HOST}
 CLIENT=$(cluster_mgr -q connect demo-sql-cassandra)
 cluster_mgr status demo-sql-cassandra --wait 5
 eval "${CLIENT} --execute 'SHOW DATABASES'"
@@ -18,7 +20,7 @@ cluster_mgr stop demo-sql-cassandra --all
 cluster_mgr remove demo-sql-cassandra
 
 sleep 15
-cluster_mgr demo eloq-kv --skip-deps --unlimited --ext-cass ${CASSANDRA_ADDR}
+cluster_mgr demo eloq-kv --skip-deps --unlimited --ext-cass ${CASSANDRA_HOST}
 CLIENT=$(cluster_mgr -q connect demo-kv-cassandra)
 cluster_mgr status demo-kv-cassandra --wait 5
 eval ${CLIENT} incr mycounter
