e0fc3bb57f1647e0eac63b892e6e76ff74724a4c
zhanghao <zhanghao997@qq.com>
2024-07-08 15:19:43 +0800

    update install_test.sh

--- concourse/install_test.sh
+++ concourse/install_test.sh
@@ -8,21 +8,19 @@ if [ "$ID" == "centos" ] || [ "$ID" == "rocky" ] || [ "$ID" == "rhel" ]; then
 elif [ "$ID" == "ubuntu" ]; then
   sudo service ssh start
 fi
-
 export CLUSTER_MGR_HOME="${HOME}/.eloqwaiter"
 bash waiter_src/concourse/install.sh
 export PATH="$PATH:$CLUSTER_MGR_HOME"
-cat ${CLUSTER_MGR_HOME}/version
 
-bash ${CLUSTER_MGR_HOME}/tests/demo.sh
+cd ${CLUSTER_MGR_HOME}
+cat version
+bash tests/demo.sh
 sleep 15
-bash ${CLUSTER_MGR_HOME}/tests/launch.sh
-
+bash tests/launch.sh
+sleep 15
+bash tests/update.sh
 sleep 15
 wget https://downloads.datastax.com/enterprise/cqlsh-astra.tar.gz
 tar -xzvf cqlsh-astra.tar.gz
 export PATH=$PATH:${PWD}/cqlsh-astra/bin
-bash ${CLUSTER_MGR_HOME}/tests/external_cass.sh 172.31.5.203
-
-sleep 15
-bash ${CLUSTER_MGR_HOME}/tests/update.sh
+bash tests/external_cass.sh 172.31.5.203
