030f34ed7c8e13a96343e256b2c09119ea357c99
zhanghao <zhanghao997@qq.com>
2024-07-08 13:59:49 +0800

    update concourse/install_test.sh

--- concourse/install_test.sh
+++ concourse/install_test.sh
@@ -4,29 +4,14 @@ set -exo pipefail
 # prepare environment
 source /etc/os-release
 if [ "$ID" == "centos" ] || [ "$ID" == "rocky" ] || [ "$ID" == "rhel" ]; then
-  #   if [ "$VERSION_ID" == "7" ]; then
-  #     sudo yum install -y epel-release && sudo yum update -y
-  #     sudo yum install -y sudo openssh-server iproute redis-tools
-  #   else
-  #     # sudo dnf install -y epel-release && sudo dnf update -y
-  #     sudo dnf install -y sudo openssh-server iproute redis
-  #   fi
   sudo /usr/sbin/sshd
-#   if [ -f "/run/nologin" ]; then
-#     sudo rm /run/nologin
-#   fi
 elif [ "$ID" == "ubuntu" ]; then
-  #   sudo apt update && DEBIAN_FRONTEND=noninteractive sudo apt install -y sudo openssh-server iproute2 redis-tools
   sudo service ssh start
 fi
-# sudo chown -R mono ${PWD}
-# sudo chown -R mono ${HOME}
 
 export CLUSTER_MGR_HOME="${HOME}/.eloqwaiter"
 bash waiter_src/concourse/install.sh
-# curl --proto '=https' --tlsv1.2 -sSf https://www.eloqdata.com/download/mono-waiter/install.sh | sh
 export PATH="$PATH:$CLUSTER_MGR_HOME"
-cat ${HOME}/.ssh/id_rsa.pub >>${HOME}/.ssh/authorized_keys
 cat ${CLUSTER_MGR_HOME}/version
 
 bash ${CLUSTER_MGR_HOME}/tests/demo.sh
