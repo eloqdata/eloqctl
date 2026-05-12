dc2bfe3bfc9a61e1d7c84082c943480dc8a0c100
starrysky <starrysky9651@outlook.com>
2026-05-12 12:36:04 +0800

    chore: add rolling_upgrade module skeleton (WIP)

---
+++ src/cluster_mgr/src/cli/task/rolling_upgrade/steps.rs
@@ -0,0 +1,5 @@
+//! Concrete Step implementations for the rolling upgrade engine.
+//!
+//! TODO: implement steps (DownloadPackages, UploadPackages, QueryTopology,
+//! FailoverMasters, StopNodes, UnpackNodes, StartLog, WaitLogReady,
+//! StartTx, WaitTxReady, StartStandby, VerifyVersion, etc.)
